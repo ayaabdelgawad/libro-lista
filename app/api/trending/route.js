d59684ff354e4651dbe1124beedf8b80c3bbf0ec
Aya Abdelgawad <aya_abdelgawad@yahoo.com>
2025-05-04 16:08:44 -0400

    trending page

---
+++ app/api/trending/route.js
@@ -0,0 +1,117 @@
+import mysql from  'mysql2/promise';
+import { NextResponse, NextRequest } from 'next/server'
+import { GetDBParams } from '@/next.config.mjs';
+
+let connectionParams = GetDBParams();
+
+export async function GET(req) {
+    try {
+        const connection = await mysql.createConnection(connectionParams);
+        // get the most popular books from this week
+        const topRatedBooksOfTheWeekQuery = `
+            SELECT
+                isbn, 
+                title,
+                author.name AS author_name,
+                AVG(rating) AS average_rating
+            FROM
+                review 
+            INNER JOIN 
+                libro
+            ON 
+                review.libro_isbn = libro.isbn
+            INNER JOIN 
+                author
+            ON
+                libro.author_id = author.id
+            WHERE
+                reviewed_at >= NOW() - INTERVAL 1 WEEK
+            GROUP BY
+                libro_isbn
+            ORDER BY
+                average_rating DESC
+            LIMIT 10;
+        `;
+        const [topRatedBooksOfTheWeek] = await connection.query(topRatedBooksOfTheWeekQuery);
+
+        // get the most reviewed books of the week
+        const mostReviewedBooksOfTheWeekQuery = `
+            SELECT
+                isbn, 
+                title,
+                author.name AS author_name,
+                COUNT(rating) AS rating_count
+            FROM
+                review 
+            INNER JOIN 
+                libro
+             ON 
+                review.libro_isbn = libro.isbn
+            INNER JOIN 
+                author
+            ON
+                libro.author_id = author.id
+            WHERE
+                reviewed_at >= NOW() - INTERVAL 1 WEEK
+            GROUP BY
+                libro_isbn
+            ORDER BY
+                rating_count DESC
+            LIMIT 10;
+        `;
+        const [mostReviewedBooksOfTheWeek] = await connection.query(mostReviewedBooksOfTheWeekQuery);
+
+        // get users that made the most reviews this week
+        const mostActiveReviewersOfTheWeekQuery = `
+            SELECT 
+                reader.username,
+                COUNT(review.rating) as review_count
+            FROM
+                reader
+            INNER JOIN
+                review
+            ON
+                reader.username = review.reviewer
+            WHERE
+                reviewed_at >= NOW() - INTERVAL 1 WEEK
+            GROUP BY
+                review.reviewer
+            ORDER BY
+                review_count DESC
+            LIMIT 10;
+        `;
+        const [mostActiveReviewersOfTheWeek] = await connection.query(mostActiveReviewersOfTheWeekQuery);
+
+        // get listas with most followers
+        const mostFollowedListasQuery = `
+            SELECT 
+                lista.id, 
+                lista.name,
+                lista.created_by,
+                COUNT(lista_followings.reader_username) AS following_count
+            FROM
+                lista
+            INNER JOIN
+                lista_followings
+            ON
+                lista.id = lista_followings.lista_id
+            GROUP BY
+                lista_followings.lista_id
+            ORDER BY 
+                following_count DESC
+            LIMIT 10;
+        `;
+        const [mostFollowedListas] = await connection.query(mostFollowedListasQuery);
+
+        connection.end();
+        return NextResponse.json({
+            topRatedBooksOfTheWeek,
+            mostReviewedBooksOfTheWeek,
+            mostActiveReviewersOfTheWeek,
+            mostFollowedListas
+        });
+    } catch (error) {
+        console.error('Error fetching data from MySQL:', error);
+        return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    }
+}
--- app/trending/page.js
+++ app/trending/page.js
@@ -1,5 +1,50 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import NavBar from "../components/NavBar";
+import Rating from '@mui/material/Rating';
+
 export default function Page() {
+
+    const [topRatedBooksOfTheWeek, setTopRatedBooksOfTheWeek] = useState([]);
+    const [mostReviewedBooksOfTheWeek, setMostReviewedBooksOfTheWeek] = useState([]);
+    const [mostActiveReviewersOfTheWeek, setMostActiveReviewersOfTheWeek] = useState([]);
+    const [mostFollowedListas, setMostFollowedListas] = useState([]);
+    
+    useEffect(() => {
+        fetch('/api/trending')
+            .then((response) => response.json())
+            .then((data) => {
+                setTopRatedBooksOfTheWeek(data.topRatedBooksOfTheWeek);
+                setMostReviewedBooksOfTheWeek(data.mostReviewedBooksOfTheWeek);
+                setMostActiveReviewersOfTheWeek(data.mostActiveReviewersOfTheWeek);
+                setMostFollowedListas(data.mostFollowedListas);
+            })
+    }, []);
+
     return (<div>
-        TRENDING PAGE
+        <NavBar/>
+        <h3 className="text-2xl">This Week's Top Rated Books</h3>
+        {topRatedBooksOfTheWeek.map((book) => 
+            <div key={book.isbn}>
+                <p>{book.author_name}'s <i>{book.title}</i></p>
+                <Rating defaultValue={book.average_rating} precision={0.01} readOnly />
+            </div>)}
+        <h3 className="text-2xl">This Week's Most Reviewed Books</h3>
+        {mostReviewedBooksOfTheWeek.map((book) => 
+            <div key={book.isbn}>
+                <b>{book.author_name}'s <i>{book.title}</i></b>
+                <p>{book.rating_count} reviews</p>
+            </div>)}
+        <h3 className="text-2xl">This Week's Most Active Reviewers</h3>
+        {mostActiveReviewersOfTheWeek.map((reviewer) => 
+            <div key={reviewer.username}>
+                <p><b>{reviewer.username}</b> - {reviewer.review_count} reviews</p>
+            </div>)}
+        <h3 className="text-2xl">Most Popular Listas of All Time</h3>
+        {mostFollowedListas.map((lista) => 
+            <div key={lista.id}>
+                <p><b>{lista.created_by}'s {lista.name}</b> - {lista.following_count} followers</p>
+            </div>)}
     </div>)
 }
